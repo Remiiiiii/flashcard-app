608bee0a4c73d0eb89978a5d73175f023af676d8
Khtran68 <khtran68@gmail.com>
2022-09-08 15:51:09 -0500

    Removed unused import

--- spring/flashapp/src/main/java/com/revature/flashapp/services/FlashcardService.java
+++ spring/flashapp/src/main/java/com/revature/flashapp/services/FlashcardService.java
@@ -1,9 +1,9 @@
 package com.revature.flashapp.services;
 
 import java.util.List;
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import com.revature.flashapp.models.Flashcards;
 import com.revature.flashapp.repository.FlashcardRepo;
 
