f92b03260f167eb24f0f19398c8a7d6896107295
Remiiiiii <tuf09783@temple.edu>
2022-09-09 16:53:49 -0400

    commit

--- spring/flashapp/src/main/java/com/revature/flashapp/models/Flashcards.java
+++ spring/flashapp/src/main/java/com/revature/flashapp/models/Flashcards.java
@@ -6,8 +6,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -34,7 +37,8 @@ public class Flashcards {
     @Column(nullable = false)
     private String description;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("flashcards")
+    @JoinColumn(nullable = false)
     private FlashcardSet flashcardSet;
     
     
