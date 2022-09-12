e8322fcca6cc7ef05a69a47309a3a1a1e64368e2
Remiiiiii <tuf09783@temple.edu>
2022-09-12 12:02:15 -0400

    added additional pages(html/css)

---
+++ spring/flashapp/src/main/resources/frontend/flashcard-dashboard/scripts.js
@@ -0,0 +1,18 @@
+let card = document.querySelector(".inside-card");
+
+card.addEventListener('click', function() {
+    card.classList.toggle('flipped');
+});
+
+let subMenu = document.getElementById("subMenu");
+
+//-------------------------------Profile Icon Menu Dropdown------------------------------
+
+// let addFlashcardElem = document.getElementById("add-flashcard-btn");
+// addFlashcardElem.addEventListener("click", () => {
+//     window.location.href = "/newflashcard-dashboard/index.html";
+// });
+
+alert("hello");
+
+
---
+++ spring/flashapp/src/main/resources/frontend/newflashcard-dashboard/scripts.js
@@ -0,0 +1,14 @@
+let card = document.querySelector(".inside-card");
+
+card.addEventListener('click', function() {
+    card.classList.toggle('flipped');
+});
+
+let subMenu = document.getElementById("subMenu");
+
+//-------------------------------Profile Icon Menu Dropdown------------------------------
+
+
+
+
+
