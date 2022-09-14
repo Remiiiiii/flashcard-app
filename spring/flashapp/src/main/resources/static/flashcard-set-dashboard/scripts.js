7d932a0266c0b8a24ac92b07cc7241f94889546a
Khtran68 <khtran68@gmail.com>
2022-09-13 22:47:18 -0500

    Display Set cards

--- spring/flashapp/src/main/resources/static/flashcard-set-dashboard/scripts.js
+++ spring/flashapp/src/main/resources/static/flashcard-set-dashboard/scripts.js
@@ -1,5 +1,5 @@
 let setContainer = document.getElementById("set-container");
-let set;
+let sets;
 let user;
 window.onload = async () => {
   let response = await fetch("http://localhost:9005/session");
@@ -12,9 +12,9 @@ window.onload = async () => {
   }
 
   user = responseBody.data;
-  items = await getAllItems();
-  console.log(items);
-  //displaySets();
+  sets = await getAllItems();
+  console.log(sets);
+  displaySets();
 }
 
 async function getAllItems(){
@@ -25,11 +25,87 @@ async function getAllItems(){
   return responseBody;
 }
 
-let card = document.querySelector(".inside-card");
+function displaySets(){
+    
+  sets.forEach(set => {
+      let parentCardElem = document.createElement("div");
+      parentCardElem.className = "card";
 
-card.addEventListener('click', function() {
-    card.classList.toggle('flipped');
-});
+      let insideCardElem = document.createElement("div");
+      insideCardElem.className = "inside-card";
+
+      insideCardElem.addEventListener('click', function() {
+        insideCardElem.classList.toggle('flipped');
+      });
+
+      let cardFaceFrontElem = document.createElement("div");
+      cardFaceFrontElem.className = "card-face card-front";
+
+      let cardFrontHeaderElem = document.createElement("div");
+      cardFrontHeaderElem.className = "card-front-header";
+
+      let imgElem = document.createElement("img");
+      imgElem.className = "front-card-logo";
+      imgElem.src = "../images/fm.jpg";
+      imgElem.alt = "app logo";
+
+      let headerFrontTextElem = document.createElement("h2");
+      headerFrontTextElem.innerText = set.title;
+
+      let cardFaceBackElem = document.createElement("div");
+      cardFaceBackElem.className = "card-face card-back";
+
+      let cardBackContentElem = document.createElement("div");
+      cardBackContentElem.className = "back-card-content";
+
+      let cardBackBodyElem = document.createElement("div");
+      cardBackBodyElem.className = "back-card-body";
+
+      let headerBackTextElem = document.createElement("h3");
+      headerBackTextElem.innerText = "Description:";
+
+      let descriptionTextElem = document.createElement("p");
+      descriptionTextElem.innerText = set.description;
+
+      let selectButtonElem = document.createElement("button");
+      selectButtonElem.className = "cards-button";
+      selectButtonElem.id = "delete-card-form";
+      selectButtonElem.innerText = "Select";
+      selectButtonElem.value = set.id;
+
+      let deleteButtonElem = document.createElement("button");
+      deleteButtonElem.className = "delete-button";
+      deleteButtonElem.id = "delete-card-form";
+      deleteButtonElem.innerText = "Delete";
+      deleteButtonElem.value = set.id;
+
+      //Front of Card Set
+      cardFrontHeaderElem.appendChild(imgElem);
+      cardFrontHeaderElem.appendChild(headerFrontTextElem);
+      cardFaceFrontElem.appendChild(cardFrontHeaderElem);
+      insideCardElem.appendChild(cardFaceFrontElem);
+      parentCardElem.appendChild(insideCardElem);
+      setContainer.appendChild(parentCardElem);
+
+      //Back of Card Set
+      cardBackBodyElem.appendChild(headerBackTextElem);
+      cardBackBodyElem.appendChild(descriptionTextElem);
+      cardBackContentElem.appendChild(cardBackBodyElem);
+      cardFaceBackElem.appendChild(cardBackContentElem);
+      insideCardElem.appendChild(cardFaceBackElem);
+
+      //Buttons
+      insideCardElem.appendChild(deleteButtonElem);
+      insideCardElem.appendChild(selectButtonElem);
+  });
+  
+}
+
+//let card = document.querySelector(".inside-card");
+
+//card.addEventListener('click', function() {
+//    card.classList.toggle('flipped');
+//});
 
 let subMenu = document.getElementById("subMenu");
 
