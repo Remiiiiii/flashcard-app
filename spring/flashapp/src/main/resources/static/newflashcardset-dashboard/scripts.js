51cc700052479d18d29b76eae5c3b2320572c7ec
Khtran68 <khtran68@gmail.com>
2022-09-14 12:19:33 -0500

    Flashcard Sets

--- spring/flashapp/src/main/resources/static/newflashcardset-dashboard/scripts.js
+++ spring/flashapp/src/main/resources/static/newflashcardset-dashboard/scripts.js
@@ -1,10 +1,62 @@
-let card = document.querySelector(".inside-card");
+let user;
 
-card.addEventListener('click', function() {
-    card.classList.toggle('flipped');
+window.onload = async () => {
+    let response = await fetch("http://localhost:9005/session");
+      
+    let responseBody = await response.json();
+  
+    if(!responseBody.successful){
+  
+      window.location = "../index.html";
+    }
+  
+    user = responseBody.data;
+}
+
+let backButton = document.getElementById("back-btn");
+
+backButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    window.location = "../flashcard-set-dashboard/index.html"
 });
 
-let subMenu = document.getElementById("subMenu");
+let saveButton = document.getElementById("save-btn");
+
+saveButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    saveSet();
+});
+
+async function saveSet(){
+    let titleText = document.getElementById("title").value;
+    let descriptionText = document.getElementById("description").value;
+
+    let response = await fetch(`http://localhost:9005/flashcardset`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "title": titleText,
+                "description": descriptionText,
+                "user": {
+                    "id": user.id
+                }
+            })
+        }
+    )
+
+    let responseBody = await response.json();
+    console.log(responseBody);
+    if(responseBody){
+        window.location = "./index.html";
+    }else{
+        console.log("ERROR: Could not create set");
+    }
+
+}
+
+//let subMenu = document.getElementById("subMenu");
 
 //-------------------------------Profile Icon Menu Dropdown------------------------------
 
